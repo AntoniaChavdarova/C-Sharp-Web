5cf7c5c6da5d39d9cc08322407a3a745ebefc26e
AntoniaChavdarova <niaaachavdarova@abv.bg>
2021-04-27 13:23:39 +0300

    Scaffold administration for categories

---
+++ ASP.NET Core/MyRecipes/Tests/MyRecipes.Services.Data.Tests/RecipeServiceTests.cs
@@ -0,0 +1,62 @@
+﻿using Moq;
+using MyRecipes.Data.Common.Repositories;
+using MyRecipes.Data.Models;
+using MyRecipes.Web.ViewModels.Recipes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyRecipes.Services.Data.Tests
+{
+    public class RecipeServiceTests
+    {
+        private readonly List<Recipe> list = new List<Recipe>();
+        private readonly Mock<IDeletableEntityRepository<Recipe>> mockRepoRecipe;
+        private readonly Mock<IDeletableEntityRepository<Ingredient>> mockRepoIngredients;
+       
+
+        public RecipeServiceTests()
+        {
+            this.mockRepoRecipe = new Mock<IDeletableEntityRepository<Recipe>>();
+            this.mockRepoIngredients = new Mock<IDeletableEntityRepository<Ingredient>>();
+           
+        }
+
+       // mockRepo.Setup(x => x.All()).Returns(this.list.AsQueryable());
+       // mockRepo.Setup(x => x.AddAsync(It.IsAny<Vote>())).Callback((Vote vote) => this.list.Add(vote));
+
+//        [Fact]
+//        public void GetAllRecipesInDBWhenGetAllMethodIsInvoke()
+//        {
+//            this.mockRepoRecipe.Setup(x => x.AllAsNoTracking()).Returns(this.list.AsQueryable());
+//            this.mockRepoRecipe.Setup(x => x.AddAsync(It.IsAny<Recipe>())).Callback((Recipe recipe) => this.list.Add(recipe));
+
+//            var service = new RecipesService(this.mockRepoRecipe.Object, this.mockRepoIngredients.Object);
+//            var recipe1 = new Recipe
+//            {
+//                Name = "Musaka",
+//            };
+
+//            var recipe2 = new Recipe
+//            {
+//                Name = "Supa",
+//            };
+
+//            var viewModel = new RecipeInListViewModel
+//            {
+//                Name = recipe1.Name,
+//            };
+//            this.list.Add(recipe1);
+//            //this.list.Add(recipe2);
+//            service.GetAll<RecipeInListViewModel>(1, 2);
+//            ;
+
+//          // Assert.Equal("Musaka" , service.)
+
+       // }
+
+    }
+}
--- ASP.NET Core/MyRecipes/Tests/MyRecipes.Services.Data.Tests/VoteServiceTests.cs
+++ ASP.NET Core/MyRecipes/Tests/MyRecipes.Services.Data.Tests/VoteServiceTests.cs
@@ -1,7 +1,49 @@
-﻿
-namespace MyRecipes.Services.Data.Tests
+﻿namespace MyRecipes.Services.Data.Tests
 {
-   public class VoteServiceTests
-    {
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Moq;
+    using MyRecipes.Data.Common.Repositories;
+    using MyRecipes.Data.Models;
+    using Xunit;
+
+    public class VoteServiceTests
+   {
+       private readonly List<Vote> list = new List<Vote>();
+
+       [Fact]
+       public async Task WhenUserVotes2TimesOnly1VoteShouldCount()
+        {
+            // Arrange
+            var mockRepo = new Mock<IRepository<Vote>>();
+            mockRepo.Setup(x => x.All()).Returns(this.list.AsQueryable());
+            mockRepo.Setup(x => x.AddAsync(It.IsAny<Vote>())).Callback((Vote vote) => this.list.Add(vote));
+            var service = new VoteService(mockRepo.Object);
+
+            // Act
+            await service.SetVoteAsync(1, "1", 1);
+            await service.SetVoteAsync(1, "1", 5);
+
+            Assert.Equal(1, this.list.Count());
+            Assert.Equal(5, this.list.First().Value);
+        }
+
+       [Fact]
+       public async Task When2UsersVoteForSameRecipeGetAverageValueCorrect()
+        {
+            var mockRepo = new Mock<IRepository<Vote>>();
+            mockRepo.Setup(x => x.All()).Returns(this.list.AsQueryable());
+           // mockRepo.Setup(x => x.To<>)
+            mockRepo.Setup(x => x.AddAsync(It.IsAny<Vote>())).Callback((Vote vote) => this.list.Add(vote));
+
+            var service = new VoteService(mockRepo.Object);
+
+            await service.SetVoteAsync(2, "1", 5);
+            await service.SetVoteAsync(2, "3", 3);
+
+            Assert.Equal(4, service.GetAverageVote(2));
+        }
     }
 }
